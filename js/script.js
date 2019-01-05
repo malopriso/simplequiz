f0fb65a6357ddc220b9e41b9e34930783622d367
malopriso <tadej.serne@gmail.com>
2019-01-05 22:41:31 +0100

    First comit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
 function submitAnswers(){
-	var total = 5;
-	var score = 0;
+var total = 5;
+var score = 0;
+//Get User Input
+var q1 = document.forms["quizForm"]["q1"].value;
+var q2 = document.forms["quizForm"]["q2"].value;
+var q3 = document.forms["quizForm"]["q3"].value;
+var q4 = document.forms["quizForm"]["q4"].value;
+var q5 = document.forms["quizForm"]["q5"].value;
+//Validation
 
-	//Get User Input
-	var q1 = document.forms["quizForm"]["q1"].value;
-	var q2 = document.forms["quizForm"]["q2"].value;
-	var q3 = document.forms["quizForm"]["q3"].value;
-	var q4 = document.forms["quizForm"]["q4"].value;
-	var q5 = document.forms["quizForm"]["q5"].value;
+for(i = 1; i <=total;i++){
+if(eval('q'+i) == null || eval('q'+i) == '') {
+alert('You missed question '+ i);
+return false;
+}
+}
 
-	//Validation
-	
+//Set Correct Answers
+var answers = ["b", "a", "d", "b", "d"];
 
-	for(i = 1; i <=total;i++){
-		if(eval('q'+i) == null || eval('q'+i) == '') {
-		alert('You missed question 1');
-		return false;
-	}
-	}
+// Check Answers
+
+for(i = 1; i <=total;i++){
+	if(eval('q' +i) == answers[i - 1]){
+	score++;
+	}	
+}
+/*
+if(q1 == answers[0]){
+	score++;
+}
+
+if(q2 == answers[1]){
+	score++;
+}
+
+if(q3 == answers[2]){
+	score++;
+}
+
+if(q4 == answers[3]){
+	score++;
+}
+
+if(q5 == answers[4]){
+	score++;
+}
+*/
+// Display Results
+var results = document.getElementById('results');
+results.innerHTML = '<h3>You scored <span>'+score+'</span> out of total <span>'+total+'</span></h3>';
+alert('You scored '+score+' out of ' +total)
+
+return false;
+
+}
